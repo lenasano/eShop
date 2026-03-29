6244975b373e729699a507b66949365d4f285cbc
lena sano <c_lena.sano@harness.io>
2026-03-29 14:45:10 -0300

    add/rename users: create 5 users

--- src/Identity.API/UsersSeed.cs
+++ src/Identity.API/UsersSeed.cs
@@ -5,32 +5,159 @@ public class UsersSeed(ILogger<UsersSeed> logger, UserManager<ApplicationUser> u
 {
     public async Task SeedAsync(ApplicationDbContext context)
     {
-        var alice = await userManager.FindByNameAsync("alice");
+        await CreateUser (
+            "ava",
+            "AvaZaczek@email.com",
+            true,
+            "Ava Zaczek",
+            "XXXXXXXXXXXX1881",
+            1,
+            "New York",
+            "U.S.",
+            "12/24",
+            "logged-in-ava", // Guid.NewGuid().ToString(),
+            "Zaczek",
+            "Ava",
+            "1234567890",
+            "98052",
+            "NY",
+            "15703 NE 61st Ct",
+            "123",
+            "Pass123$"
+        );
 
-        if (alice == null)
+        await CreateUser (
+            "bob",
+            "BobYates@email.com",
+            true,
+            "Bob Yates",
+            "XXXXXXXXXXXX1881",
+            1,
+            "New York",
+            "U.S.",
+            "12/24",
+            "logged-in-bob", // Guid.NewGuid().ToString(),
+            "Yates",
+            "Bob",
+            "1234567890",
+            "98052",
+            "NY",
+            "15703 NE 61st Ct",
+            "456",
+            "Pass123$"
+        );
+
+        await CreateUser (
+            "charu",
+            "CharuZutshi@email.com",
+            true,
+            "Charu Zutshi",
+            "XXXXXXXXXXXX1881",
+            1,
+            "New York",
+            "U.S.",
+            "12/24",
+            "logged-in-charu", // Guid.NewGuid().ToString(),
+            "Zutshi",
+            "Charu",
+            "1234567890",
+            "98052",
+            "NY",
+            "15703 NE 61st Ct",
+            "789",
+            "Pass123$"
+        );
+
+        await CreateUser (
+            "danja",
+            "DanjaWede@email.com",
+            true,
+            "Danja Wede",
+            "XXXXXXXXXXXX1881",
+            1,
+            "New York",
+            "U.S.",
+            "12/24",
+            "logged-in-danja", // Guid.NewGuid().ToString(),
+            "Wede",
+            "Danja",
+            "1234567890",
+            "98052",
+            "WA",
+            "15703 NE 61st Ct",
+            "012",
+            "Pass123$"
+        );
+
+        await CreateUser (
+            "enam",
+            "EnamVida@email.com",
+            true,
+            "Enam Vida",
+            "XXXXXXXXXXXX1881",
+            1,
+            "New York",
+            "U.S.",
+            "12/24",
+            "logged-in-enam", // Guid.NewGuid().ToString(),
+            "Vida",
+            "Enam",
+            "1234567890",
+            "98052",
+            "NY",
+            "15703 NE 61st Ct",
+            "345",
+            "Pass123$"
+        );
+    }
+
+    private async Task CreateUser(
+        string userName,
+        string email,
+        bool emailConfirmed,
+        string cardHolderName,
+        string cardNumber,
+        int cardType,
+        string city,
+        string country,
+        string expiration,
+        string id,
+        string lastName,
+        string name,
+        string phoneNumber,
+        string zipCode,
+        string state,
+        string street,
+        string securityNumber,
+        string password
+    )
+    {
+        var user = await userManager.FindByNameAsync(userName);
+
+        if (user == null)
         {
-            alice = new ApplicationUser
+            user = new ApplicationUser
             {
-                UserName = "alice",
-                Email = "AliceSmith@email.com",
-                EmailConfirmed = true,
-                CardHolderName = "Alice Smith",
-                CardNumber = "XXXXXXXXXXXX1881",
-                CardType = 1,
-                City = "Redmond",
-                Country = "U.S.",
-                Expiration = "12/24",
-                Id = Guid.NewGuid().ToString(),
-                LastName = "Smith",
-                Name = "Alice",
-                PhoneNumber = "1234567890",
-                ZipCode = "98052",
-                State = "WA",
-                Street = "15703 NE 61st Ct",
-                SecurityNumber = "123"
+                UserName = userName,
+                Email = email,
+                EmailConfirmed = emailConfirmed,
+                CardHolderName = cardHolderName,
+                CardNumber = cardNumber,
+                CardType = cardType,
+                City = city,
+                Country = country,
+                Expiration = expiration,
+                Id = id,
+                LastName = lastName,
+                Name = name,
+                PhoneNumber = phoneNumber,
+                ZipCode = zipCode,
+                State = state,
+                Street = street,
+                SecurityNumber = securityNumber
             };
 
-            var result = await userManager.CreateAsync(alice, "Pass123$");
+            var result = await userManager.CreateAsync(user, password);
 
             if (!result.Succeeded)
             {
@@ -39,60 +166,16 @@ public class UsersSeed(ILogger<UsersSeed> logger, UserManager<ApplicationUser> u
 
             if (logger.IsEnabled(LogLevel.Debug))
             {
-                logger.LogDebug("alice created");
+                logger.LogDebug("{userName} created", userName);
             }
         }
         else
         {
             if (logger.IsEnabled(LogLevel.Debug))
             {
-                logger.LogDebug("alice already exists");
+                logger.LogDebug("{userName} already exists", userName);
             }
         }
-
-        var bob = await userManager.FindByNameAsync("bob");
-
-        if (bob == null)
-        {
-            bob = new ApplicationUser
-            {
-                UserName = "bob",
-                Email = "BobSmith@email.com",
-                EmailConfirmed = true,
-                CardHolderName = "Bob Smith",
-                CardNumber = "XXXXXXXXXXXX1881",
-                CardType = 1,
-                City = "Redmond",
-                Country = "U.S.",
-                Expiration = "12/24",
-                Id = Guid.NewGuid().ToString(),
-                LastName = "Smith",
-                Name = "Bob",
-                PhoneNumber = "1234567890",
-                ZipCode = "98052",
-                State = "WA",
-                Street = "15703 NE 61st Ct",
-                SecurityNumber = "456"
-            };
-
-            var result = await userManager.CreateAsync(bob, "Pass123$");
-
-            if (!result.Succeeded)
-            {
-                throw new Exception(result.Errors.First().Description);
-            }
-
-            if (logger.IsEnabled(LogLevel.Debug))
-            {
-                logger.LogDebug("bob created");
-            }
-        }
-        else
-        {
-            if (logger.IsEnabled(LogLevel.Debug))
-            {
-                logger.LogDebug("bob already exists");
-            }
-        }
+        
     }
 }
