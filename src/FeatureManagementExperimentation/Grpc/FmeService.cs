6471b718ec22eb917f6d311a3f52e62909603e19
lena sano <c_lena.sano@harness.io>
2026-04-15 03:05:06 -0300

    Harness FME: Correctly implement FME client as a singleton

--- src/FeatureManagementExperimentation/Grpc/FmeService.cs
+++ src/FeatureManagementExperimentation/Grpc/FmeService.cs
@@ -7,23 +7,25 @@ using Splitio.Services.Logger;
 
 namespace FeatureManagementExperimentation.Grpc;
 
-public class FmeService : Fme.FmeBase
+public sealed class FmeService : Fme.FmeBase
 {
     private static readonly string FME_SERVERSIDE_SDK_KEY = "FME_SERVERSIDE_SDK_KEY";
     private static readonly string TRAFFIC_TYPE = "user";
 
     private readonly ILogger<FmeService> _logger;
-    private readonly ISplitClient? _fmeSdkClient;
+    private static ISplitClient? _fmeSdkClient;
 
     public FmeService(ILogger<FmeService> logger)
     {
         _logger = logger;
-        _logger.LogDebug($"Initializing FmeService. This service should be added as a singleton, so this message should be logged just once (each time the application starts).");
+        _logger.LogDebug("Creating a new instance of FmeService.");
 
         try
         {
+            _logger.LogDebug($"Initializing FME SDK client: {_fmeSdkClient is null}");
+
             // set up FME API connection
-            _fmeSdkClient = 
+            _fmeSdkClient ??= 
                 new SplitFactory( 
                     Environment.GetEnvironmentVariable(FME_SERVERSIDE_SDK_KEY),
                     new ConfigurationOptions {
